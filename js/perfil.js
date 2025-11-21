a560417386e68e7cc1d48d3e0cf36b484059e587
Samantha-Ramirez <samantha.p.ramirez.y@gmail.com>
2025-11-20 21:18:58 -0400

    perfil con información dinámica

--- js/perfil.js
+++ js/perfil.js
@@ -1,37 +1,42 @@
 
-function getJoinedArray(array, sep) {
-    if(Array.isArray(array)) {
-        array = array.join(sep);
-    }
-    return array;
-}
+import { getJoinedArray, getImgElement, doOnload } from './utils.js';
 
-window.onload = function() {
-    // HEAD
-    const titleElement = document.getElementById('title');
-    titleElement.textContent = perfil.nombre;
+document.addEventListener('DOMContentLoaded', function() {
+    const runLogic = function(profileId, configId) {
+        if(typeof perfil !== 'undefined' && typeof config !== 'undefined' && perfil.ci === profileId) {
+            // HEAD
+            const titleElement = document.getElementById('title');
+            titleElement.textContent = perfil.nombre;
 
-    // CONTENT
-    const nameElement = document.querySelector('.perfil-content-name');
-    nameElement.textContent = perfil.nombre;
+            // CONTAINER
+            const imgElement = document.querySelector('.perfil-img');
+            const profileImg = getImgElement(perfiles.find(p => p.ci === profileId));
+            imgElement.appendChild(profileImg);
 
-    const descriptionElement = document.querySelector('.perfil-content-description');
-    descriptionElement.textContent = perfil.descripcion;
+            // CONTENT
+            const nameElement = document.querySelector('.perfil-content-name');
+            nameElement.textContent = perfil.nombre;
 
-    // CONTENT DATA
-    const dataIds = ['color', 'book', 'music', 'videogames', 'langs'];
-    const valueIds = ['color', 'libro', 'musica', 'video_juego', 'lenguajes'];
-    for(let id = 0; id < dataIds.length; id++) {
-        const dataLabelElement = document.getElementById(dataIds[id]);
-        const dataValueElement = dataLabelElement.nextElementSibling;
-        dataLabelElement.textContent = config[valueIds[id]];
-        dataValueElement.textContent = getJoinedArray(perfil[valueIds[id]], ', ');
-    }
+            const descriptionElement = document.querySelector('.perfil-content-description');
+            descriptionElement.textContent = perfil.descripcion;
 
-    // CONTENT CONTACT
-    const contactElement = document.querySelector('.perfil-content-contact');
-    const emailElement = document.createElement('a');
-    emailElement.textContent = perfil.email;
-    emailElement.href = 'mailto:' + perfil.email;
-    contactElement.innerHTML = config.email.replace('[email]', emailElement.outerHTML);
-}
+            // CONTENT DATA
+            const dataIds = { 'color': 'color', 'book': 'libro', 'music': 'musica', 'videogames': 'video_juego', 'langs': 'lenguajes' };
+            Object.entries(dataIds).forEach(([key, value]) => {
+                const dataLabelElement = document.getElementById(key);
+                const dataValueElement = dataLabelElement.nextElementSibling;
+                dataLabelElement.textContent = config[value];
+                dataValueElement.textContent = getJoinedArray(perfil[value], ', ');
+            });
+
+            // CONTENT CONTACT
+            const contactElement = document.querySelector('.perfil-content-contact');
+            const emailElement = document.createElement('a');
+            emailElement.textContent = perfil.email;
+            emailElement.href = `mailto:${perfil.email}`;
+            contactElement.innerHTML = config.email.replace('[email]', emailElement.outerHTML);
+        }
+    };
+
+    doOnload(runLogic);
+});
